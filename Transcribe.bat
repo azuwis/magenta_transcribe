1679f8a48339fba9beef5503a1a4e5383244d2e8
Zhong Jianxin <azuwis@gmail.com>
2019-07-10 15:50:13 +0800

    Support args for Transcribe.bat

--- Transcribe.bat
+++ Transcribe.bat
@@ -1,8 +1,10 @@
 @echo off
 call "%~dp0scripts\env.bat"
 "%WINPYDIR%\python.exe" "%WINPYDIR%\Scripts\transcribe.py" %*
+IF %0 == "%~0" (
 echo.
 echo Done.
 echo Press any key to exit...
 pause >nul
+)
 
--- transcribe.py
+++ transcribe.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python
 
 import os, sys
-import tkinter as tk
-from tkinter import filedialog
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    root.withdraw()
+    have_args = len(sys.argv) > 1
 
-    files = filedialog.askopenfilenames(filetypes = [('wav files', '*.wav *.wave')])
-    if files:
+    if have_args:
+        argv = sys.argv
+    else:
+        import tkinter as tk
+        from tkinter import filedialog
+        root = tk.Tk()
+        root.withdraw()
+        files = filedialog.askopenfilenames(filetypes = [('wav files', '*.wav *.wave')])
+        argv = (sys.argv[0],) + root.tk.splitlist(files)
+
+    if have_args or files:
         import tensorflow.compat.v1 as tf
         from magenta.models.onsets_frames_transcription.onsets_frames_transcription_transcribe import main, FLAGS
 
         model_dir = os.path.join(os.path.dirname(__file__), '../../train')
         FLAGS.model_dir = model_dir
 
-        tf.app.run(main = main, argv = (sys.argv[0],) + root.tk.splitlist(files))
+        tf.app.run(main = main, argv = argv)
